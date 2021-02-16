b198f859ba9d1f8e1e23b886de6d839e298d9b59
ukeh <ukeh.hari@gmail.com>
2021-02-16 11:08:17 +0530

    second commit

--- app.js
+++ app.js
@@ -1,34 +1,61 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+var cors = require('cors');
+var chalk = require('chalk');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+var whitelist = ['http://localhost:3000']
+var corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    }
+    else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+}
+
+var mongooseOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}
+
+
+
+require('dotenv').config();
+
+
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//MongoDB connect
+mongoose.connect(process.env.DB_CONNECTION, mongooseOptions)
+  .then(() => {
+    console.log(chalk.green("Connected Successfully to port 27017"));
+  }).catch((error) => {
+    console.log(chalk.red(error));
+  });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.use(cors(corsOptions));
+
+
+
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
